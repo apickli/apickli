1fb9f39ea2bd2015ef0d37a5d8901ed0313d9bf8
seymen <oseymen@gmail.com>
2015-05-11 20:41:13 +0100

    v1.0.0 release

--- example-project/features/step_definitions/httpbin.js
+++ example-project/features/step_definitions/httpbin.js
@@ -9,4 +9,23 @@ module.exports = function() {
 		this.apickli = new apickli.Apickli('http', 'httpbin.org');
 		callback();
 	});
+
+	var subtractionResult;
+
+	this.When(/^I subtract (.*) from (.*)$/, function(variable1, variable2, callback) {
+		var value1 = this.apickli.getGlobalVariable(variable1); 
+		var value2 = this.apickli.getGlobalVariable(variable2);
+		subtractionResult = value2 - value1;
+
+		callback();
+	});
+
+	this.Then(/^result should be (\d+)$/, function(result, callback) {
+		if (subtractionResult == result) {
+			callback();
+		} else {
+			callback.fail(subtractionResult + ' is not equal to ' + result);
+		}
+	});
+
 };
