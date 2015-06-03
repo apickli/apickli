5ba7eb93075e102002d55e2947d9ba01b971a7c9
seymen <oseymen@gmail.com>
2015-06-03 14:45:13 +0100

    v1.0.2 release

--- source/apickli/generate-npm.sh
+++ source/apickli/generate-npm.sh
@@ -3,11 +3,11 @@ mkdir ./npm
 
 cp ./apickli.js ./npm/apickli.js
 cp ./apickli-gherkin.js ./npm/apickli-gherkin.js
-cp ./package.json ./npm/package.json
+cp ../package.json ./npm/package.json
 cp ../../LICENSE ./npm/LICENSE
 cp ../../README.md ./npm/README.md
 
 cd ./npm
-npm publish
+npm publish 
 
 rm -rf ./npm
