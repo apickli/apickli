8385894ed6c821fbd5c098f9e0808241bd82a37d
Ozan Seymen <oseymen@gmail.com>
2016-01-11 12:58:09 +0000

    removing apickli reference from apickli-gherkin step definitions

--- source/apickli/apickli-gherkin.js
+++ source/apickli/apickli-gherkin.js
@@ -1,8 +1,6 @@
 /* jslint node: true */
 'use strict';
 
-var apickli = require('apickli');
-
 module.exports = function () {
     this.Given(/^I set (.*) header to (.*)$/, function (headerName, headerValue, callback) {
         this.apickli.addRequestHeader(headerName, headerValue);
