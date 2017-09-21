bcdae8d51d6bf7b550caed9a73c223ba14b3fb46
MRJ <rjmadhu04@gmail.com>
2017-09-21 01:32:54 -0700

    Update Gulpfile.js
    Generate json/html reports

--- source/Gulpfile.js
+++ source/Gulpfile.js
@@ -1,12 +1,46 @@
 const gulp = require('gulp');
 const cucumber = require('gulp-cucumber');
 const eslint = require('gulp-eslint');
+const reporter = require('cucumber-html-reporter');
+const runSequence = require('run-sequence');
+const fsp = require('fs-promise');
+
+
+gulp.task('run-test', function() {
+    runSequence('onPrepare',
+        'test',
+        'reportHtml');
+});
+
+gulp.task('onPrepare', function() {
+    return fsp.emptyDir('reports/json')
+        .then(function() {
+            return fsp.emptyDir('reports/html');
+        });
+});
+
+
+gulp.task('reportHtml', function() {
+    const options = {
+        theme: 'bootstrap',
+        jsonFile: 'reports/json/reports.json',
+        output: 'reports/html/reports.html',
+        reportSuiteAsScenarios: true,
+        launchReport: false,
+        metadata: {
+            'App Version': '0.0.3',
+         },
+    };
+    reporter.generate(options);
+});
 
 gulp.task('test', function() {
     const options = {
         'steps': 'test/features/step_definitions/*.js',
         'support': 'test/features/support/init.js',
         'tags': '@core',
+        'format': 'json:reports/json/reports.json',
+
     };
 
     return gulp.src('test/features/*')
@@ -16,6 +50,7 @@ gulp.task('test', function() {
 gulp.task('console-test', function() {
     const options = {
         'steps': 'test/features/step_definitions/*.js',
+        'support': 'test/features/support/init.js',
         'tags': '@console',
         'format': 'pretty',
     };
