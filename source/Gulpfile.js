b26f2ddeef50dbf767249053c3d762a97a98de43
Aparna Gopalakrishnan <13162138+aparna-gopalakrishnan@users.noreply.github.com>
2018-09-13 17:49:58 +0100

    Gulp and package.json for report change

--- source/Gulpfile.js
+++ source/Gulpfile.js
@@ -1,6 +1,55 @@
 const gulp = require('gulp');
 const cucumber = require('gulp-cucumber');
 const eslint = require('gulp-eslint');
+const report = require('cucumber-html-reporter');
+var jsonReportPath = 'test/reports/json/';
+var htmlReportPath = 'test/reports/html/';
+const fs = require('fs-extra');
+
+const cucumberReportOpts ={
+    theme: 'bootstrap',
+    jsonDir: jsonReportPath,
+    output: htmlReportPath+'/report.html',
+    brandTitle: 'Demo Tests Results Report',
+    reportSuiteAsScenarios: true,
+    launchReport: false,
+    storeScreenShots: true,
+    ignoreBadJsonFile: true,
+    };
+
+gulp.task('createFolders',(done)=>{
+    new Promise(function(resolve,reject){
+        try{
+            fs.mkdirsSync(jsonReportPath);
+            fs.mkdirsSync(htmlReportPath);
+
+            resolve();
+        }
+        catch (e) {
+            console.log(e);
+            done();
+        }
+    }).then(function(){
+        done();
+    })
+});
+
+gulp.task('report', (done) => {
+
+    new Promise(function(resolve,reject){
+        try{
+            report.generate(cucumberReportOpts);
+            resolve();
+        }
+        catch (e) {
+            console.log(e);
+            done();
+        }
+    }).then(function(){
+        done();
+    })
+
+});
 
 gulp.task('test', function() {
     const options = {
